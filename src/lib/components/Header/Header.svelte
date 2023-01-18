f49ad1f922050b8f86b0b24357ff385cea84d9cf
Niag Alves <niag.souza1993@hotmail.com>
2023-01-17 22:30:59 -0300

    feat: add header component

---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,9 @@
+<header class="p-4 h-16">
+  <div class="max-w-3xl mx-auto flex items-center justify-start">
+    <a data-sveltekit-preload-data="tap" href="/" class="cursor-pointer border-none no-underline hover:text-orange-600 transition duration-150 ease-in-out">
+      <b class="font-bold font-['Lora'] italic text-[22px]">
+        NiagAlves
+      </b>
+    </a>
+  </div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+
+  import Header from '$lib/components/Header/Header.svelte';
+  import Footer from '$lib/components/Footer/Footer.svelte';
 </script>
 
+<Header />
 <slot />
+<Footer />
