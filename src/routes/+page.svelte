abba7e9ee6b375a9cbdb70762e33274b4980bb24
Niag Alves <niag.souza1993@hotmail.com>
2023-01-22 00:36:01 -0300

    feat: add skills section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Intro from './components/Intro.svelte';
 	import About from './components/About.svelte';
+	import Skills from './components/Skills.svelte';
 </script>
 
 <Intro />
 <About />
+<Skills />
