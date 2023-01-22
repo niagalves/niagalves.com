8dc1b20a856dc9d6b5e9bdbc34e730914a250ab8
Niag Alves <niag.souza1993@hotmail.com>
2023-01-22 01:59:47 -0300

    feat: add header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,1 +1,49 @@
-<header class="p-4 h-16">header</header>
+<script lang="ts">
+	type List = {
+		title: string;
+		url: string;
+	};
+
+	const list: List[] = [
+		{
+			title: 'Artigos',
+			url: '/'
+		},
+		{
+			title: 'Projetos',
+			url: '/'
+		},
+		{
+			title: 'Contato',
+			url: '/'
+		}
+	];
+</script>
+
+<header class="p-4 max-w-4xl mx-auto flex justify-between items-center">
+	<a href="/" title="Início" class="flex items-center flex-row gap-1">
+		<span class="font-bold inline-flex">
+			Niag
+			<span class="font-bold">Alves</span>
+		</span>
+		<span class="text-cyan-400 font-black"> /> </span>
+	</a>
+	<nav>
+		{#if list}
+			<ul class="flex items-center gap-4 flex-wrap">
+				{#each list as item}
+					<li>
+						<a
+							href={item.url}
+							title={item.title}
+							class="hover:border-cyan-400 capitalize transition duration-700
+            ease-in-out border-transparent border-b-2 text-sm font-medium"
+						>
+							{item.title}
+						</a>
+					</li>
+				{/each}
+			</ul>
+		{/if}
+	</nav>
+</header>
