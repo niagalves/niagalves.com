a589758756f56e79302116f45c0b5bbf60e3d4d8
Niag Alves <niag.souza1993@hotmail.com>
2023-01-22 03:03:04 -0300

    fix: responsive intro component

--- src/routes/components/Intro.svelte
+++ src/routes/components/Intro.svelte
@@ -7,7 +7,7 @@
 <Section>
 	<div class="flex justify-between md:flex-row flex-col items-center">
 		<img
-			class="w-36 md:w-44 mx-auto rounded-full md:h-44 h-36 mb-8 relative top-[10px]"
+			class="w-36 md:w-44 rounded-full md:h-44 h-36 mb-8 relative top-[10px]"
 			src="/niagalves.png"
 			alt="niagalves"
 		/>
