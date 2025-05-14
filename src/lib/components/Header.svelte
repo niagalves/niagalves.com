4c7b4b95a4e99b8fd34b9017763fa122e5507671
niagalves <niag.souza1993@hotmail.com>
2025-05-14 18:51:20 -0300

    feat: add header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,29 @@
-<script>
-	import Container from './Container.svelte';
-	import Logo from './Logo.svelte';
+<script lang="ts">
+	import { page } from '$app/state';
+	import links from '../../utils/links';
 </script>
 
 <header>
-	<Container>
-		<a href="/" title="Início">
-			<Logo />
-		</a>
-	</Container>
+	<div class="mx-auto flex w-full max-w-7xl items-center justify-end px-4 py-6">
+		<nav>
+			<ul class="flex gap-6">
+				{#each links as item}
+					<li>
+						<a
+							href={item.href}
+							title={item.title}
+							class={`hover:text-primary border-0 text-base font-semibold transition ease-in hover:border-b-2 
+							${
+								page.url.pathname === item.href || page.url.pathname.startsWith(item.href + '/')
+									? 'text-primary border-b-2'
+									: ''
+							}`}
+						>
+							{item.title}
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+	</div>
 </header>
---
+++ src/utils/links.ts
@@ -0,0 +1,16 @@
+const links = [
+	{
+		title: 'Início',
+		href: '/'
+	},
+	{
+		title: 'Blog',
+		href: '/blog'
+	},
+	{
+		title: 'Links',
+		href: '/links'
+	}
+];
+
+export default links;
