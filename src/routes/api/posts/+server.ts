789ab8943aae4ce0b55a77060fb3edda62cf947e
niagalves <niag.souza1993@hotmail.com>
2025-05-16 22:59:19 -0300

    refactor: get files

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,34 +1,36 @@
-import fs from 'fs';
-import path from 'path';
 import matter from 'gray-matter';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const config = {
-	runtime: 'nodejs18.x'
-};
-
-export const GET: RequestHandler = ({ url }) => {
+export const GET: RequestHandler = async ({ url }) => {
 	const page = parseInt(url.searchParams.get('page') || '1');
 	const pageSize = parseInt(url.searchParams.get('pageSize') || '5');
-	const postsDir = path.resolve('static/posts');
-	const files = fs
-		.readdirSync(postsDir)
-		.filter((file) => file.endsWith('.svx') || file.endsWith('.md'));
 
-	const allPosts: Post[] = files.map((file) => {
-		const slug = file.replace(/\.(svx|md)$/, '');
-		const fileContent = fs.readFileSync(path.join(postsDir, file), 'utf-8');
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const postFiles: any = import.meta.glob('/static/posts/*.{md,svx}', {
+		query: '?raw',
+		import: 'default'
+	});
+
+	const allPosts: Post[] = [];
+
+	for (const path in postFiles) {
+		const fileContent = await postFiles[path]();
+		const slug =
+			path
+				.split('/')
+				.pop()
+				?.replace(/\.(md|svx)$/, '') || '';
 		const { data } = matter(fileContent);
 
-		return {
+		allPosts.push({
 			slug,
 			...data
-		} as Post;
-	});
+		} as Post);
+	}
 
 	allPosts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
-	const totalPosts = allPosts?.length;
+	const totalPosts = allPosts.length;
 	const totalPages = Math.ceil(totalPosts / pageSize);
 
 	const start = (page - 1) * pageSize;
