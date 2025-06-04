e409bb8a5d564ff3a05f33d7ba6a7bcc5f21aa9b
niagalves <niag.souza1993@hotmail.com>
2025-06-03 21:38:15 -0300

    feat: add nuxtjs

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Footer, Header } from '#components';
+</script>
+
+<template>
+  <Header />
+  <main class="bg-teal-600 min-h-screen">
+    <slot />
+  </main>
+  <Footer />
+</template>
